bf5f025eb258bcc2e17f01b52706e1676b6a35a4
RahulVyas12 <vyasr7972@gmail.com>
2026-02-21 17:50:44 +0530

    add Register page and update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
+import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            
         </Routes>
     );
 }

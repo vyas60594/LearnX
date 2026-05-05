840e55ae5d68917af3dec7a5813bcea5f7b5c36f
RajPurohit-hub <vyas60594@gmail.com>
2026-05-05 21:48:03 +0530

    improvement

--- backend/get_cols.js
+++
@@ -1,1 +0,0 @@
-import pool from './src/config/db.js'; pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'users'").then(res => { console.log(res.rows); process.exit(0); });
